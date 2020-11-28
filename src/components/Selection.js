3dcd6ed1f1df9841d54f22dca74eb8fdf8108bc6
saloni-hub <pandeysaloni4july@gmail.com>
2020-11-28 18:28:48 +0530

    color

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React from 'react';
 
+class Selection extends React.Component {
+    constructor(applyColor) {
+        super(applyColor);
+    }
+    render() {
+        return (
+            <div className='fix-box' style={{background:''}}>
+
+            </div>
+        )
+    }
+}
+
+export default Selection;
